b1feb8bcaf2c6ba69766425c23a9914006147985
Maryam Ehab <maryammehab@gmail.com>
2019-03-17 00:05:35 +0200

    final ?

--- assignment1.py
+++ assignment1.py
@@ -1,32 +1,28 @@
 from string import punctuation
 from collections import Counter
 import nltk
-import os
-from nltk import RegexpTokenizer, ngrams
-from nltk.corpus import wordnet
 import re
-import string
-from nltk.stem.porter import PorterStemmer
-from nltk.stem.lancaster import LancasterStemmer
-from nltk.stem import SnowballStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
 
 
-def strip_punctuation(s):
-    return ''.join(c for c in s if c not in punctuation)
+#convert from docx to txt
+''''
+from docx import Document
 
-def remove_punctuation(value):
-    result = ""
-    for c in value:
-        # If char is not punctuation, add it to the result.
-        if c not in string.punctuation:
-            result += c
-    return result
+document = Document()
+document.save('mn.docx')
+
+import docx
+
+doc = docx.Document('mn.docx')
+fullText = []
+for para in doc.paragraphs:
+    fullText.append(para.text)
+print(fullText)'''
 
 file=open('C:\/Users\Mariam\Desktop\data.txt','r')
 f = file.read()
 sentence = nltk.sent_tokenize(f)
+print("Sentences: ")
 print(sentence)
 words = []
 words2 = []
@@ -34,50 +30,72 @@ for i in sentence:
     #word = nltk.word_tokenize(i)
     word = re.sub(r'\d+', '', i)
     words.append(word)
+print("Words without numbers: ")
 print(word)
-print("mariamm")
-print(words)
-print(words2)
+
+
+def removepunc(text):
+    result = ""
+    for i in text:
+        if i not in punctuation:
+            result += i
+    return result
+
+
 withoutpunc = []
 for i in range(len(words)):
-    x = remove_punctuation(words[i])
+    x = removepunc(words[i])
     print(x)
     withoutpunc.append(x)
+print("Words without punctuation: ")
 print(withoutpunc)
+
 for i in withoutpunc:
     word = nltk.word_tokenize(i)
     words2.append(word)
-print("hnaaaaaaaaaaaaa", words2)
+print("Words: ")
+print(words2)
 
 onelist = []
 for x in words2:
     for y in x:
         onelist.append(y)
-print("onelist  ",onelist)
-'''
-bigrams= ngrams(words2, 2)
-trigrams=ngrams(words2,3)
-for i in trigrams:
-    print(trigrams,"trrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")'''
-new_trigrams = []
-newbri = []
-c = 0
-cc = 0
+print("onelist : ",onelist)
 
-while cc < len(onelist) - 1:
-    newbri.append((onelist[cc], onelist[cc+1]))
-    cc += 1
-print(newbri)
+trigrams = []
+bigrams = []
+t = 0
+b = 0
+for i in range(len(onelist)):
+    if b < i - 1:
+        bigrams.append((onelist[b], onelist[b+1]))
+        b += 1
+print("Bigram: ", bigrams)
 
-while c < len(onelist) - 2:
-    new_trigrams.append((onelist[c], onelist[c+1], onelist[c+2]))
-    c += 2
-print(new_trigrams)
+for i in range(len(onelist)):
+    if t < i - 1:
+        trigrams.append((onelist[t], onelist[t+1], onelist[t+2]))
+        t += 2
+print("Trigram: ", trigrams)
 
-trigram = Counter(new_trigrams)
-print("tttttttttttttttttt",trigram)
+trigram = Counter(trigrams)
+print(trigram)
 countwords = Counter(onelist)
 print(countwords)
 
+
+def probability(a, b):
+    p1 = a*b
+    p2 = p1 / a
+
+
 ff = len(onelist)
 print(ff)
+#probability()
+
+
+bigrams= nltk.ngrams(onelist, 2)
+t = []
+trigrams = nltk.ngrams(onelist, 3)
+t.append(trigrams)
+print(trigrams,"trrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
